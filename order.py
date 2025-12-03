4a4246935bd942ca45bfa08487976cde1f3c4f25
George <gnm7208@gmail.com>
2025-12-04 00:59:55 +0300

    feat: add Order initialization and all() class method

--- order.py
+++ order.py
@@ -1,2 +1,12 @@
 class Order:
     _all = []
+    
+    def __init__(self, customer, coffee, price):
+        self.customer = customer
+        self.coffee = coffee
+        self.price = price
+        Order._all.append(self)
+    
+    @classmethod
+    def all(cls):
+        return list(cls._all)
