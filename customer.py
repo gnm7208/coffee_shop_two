0ae703da5f1b32487c18d067f14f7527ae36ab67
George <gnm7208@gmail.com>
2025-12-04 00:59:34 +0300

    feat: add Customer name validation and properties

--- customer.py
+++ customer.py
@@ -1,2 +1,15 @@
 class Customer:
-    pass
+    def __init__(self, name):
+        self.name = name
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Customer name must be a string.")
+        if not (1 <= len(value) <= 15):
+            raise ValueError("Customer name must be between 1 and 15 characters.")
+        self._name = value
